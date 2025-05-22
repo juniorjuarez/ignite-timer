6bd9ccc20fc331759e3d2cac5ea002d7f989c5ad
Jose Juarez <juniojuarez@yahoo.com.br>
2025-05-22 13:54:10 -0300

    Update styles.ts

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 10rem);
+  // height: calc(100vh - 10rem);
   background: ${(props) => props.theme["gray-800"]};
   margin: 5rem auto;
   padding: 2.5rem;
